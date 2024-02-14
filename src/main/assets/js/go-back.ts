3f1fee193a70cb92c1090357efdeb4300eaaeafb
gabriellemagruder <gabrielle.magruder@solirius.com>
2024-02-14 13:54:53 +0000

    Reverse changes, back button will be fixed in different branch.

--- src/main/assets/js/go-back.ts
+++ src/main/assets/js/go-back.ts
@@ -1,21 +1,7 @@
-import { CASE_SEARCH_URL, CHECK_YOUR_ANSWERS, DATA_VERIFICATION, START_HOME, UPLOAD_DOCUMENT } from '../../steps/urls';
-
 const backLink: HTMLAnchorElement | null = document.querySelector('.govuk-back-link');
 if (backLink) {
   backLink.onclick = function (e) {
-    console.log('TEST');
     e.preventDefault();
-    if (location.pathname === CASE_SEARCH_URL) {
-      console.log('HERE');
-      location.pathname = START_HOME;
-    } else if (location.pathname === DATA_VERIFICATION) {
-      location.pathname = CASE_SEARCH_URL;
-    } else if (location.pathname === UPLOAD_DOCUMENT) {
-      location.pathname = DATA_VERIFICATION;
-    } else if (location.pathname === CHECK_YOUR_ANSWERS) {
-      location.pathname = UPLOAD_DOCUMENT;
-    } else {
-      history.go(-1);
-    }
+    history.go(-1);
   };
 }
