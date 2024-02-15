4685b6af13364633a0f231f62719501fb5c66d87
gabriellemagruder <gabrielle.magruder@solirius.com>
2024-02-15 12:56:32 +0000

    Make content changes to user interface on upload documents page

--- src/main/steps/upload-documents/content.test.ts
+++ src/main/steps/upload-documents/content.test.ts
@@ -2,29 +2,34 @@ import { cy, en, form, generateContent } from './content';
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 const englishContent = () => ({
-  serviceName: 'DSS Update Case',
-  title: 'Upload documents',
+  serviceName: 'Update an existing First-tier Tribunal case',
+  title: 'Add information to a case',
   paragraph1:
-    'You can upload additional documents, files or other evidence to support your case. You can also add information below that is relevant to the case.',
-  uploadedDocumentsSubTitle: 'Uploaded documents can include:',
+    'Please provide any additional relevant information that you would like to add to this case. Don\'t include any personal financial information, such as your banking details.',
+  additionalDocumentsHeading: 'Additional documents',
+  paragraph2: 'You may want to submit other documents, files, or evidence that provides the Tribunal with more information about your case.',
+  paragraph3: 'This will depend on the nature of your appeal, application or claim. Specific examples can be found in your completed tribunal form.',
+  uploadedDocumentsSubTitle: 'These may include:',
   uploadDocumentsBullets: [
     'copies of any relevant correspondence or documentation',
+    'audio or video recordings',
     'written statements',
     'related reports',
   ],
-  caseRelevancySubTitle: 'For each document uploaded you must state why it is relevant to the case.',
+  caseRelevancySubTitle: 'For each piece of additional information or evidence you are submitting, please state why it is relevant to your case.',
   fileUploadRequirementsLabel: 'File upload requirements',
   uploadHelpBullets: [
-    'File formats: MS Word, MS Excel, PDF, JPG, PNG, TXT, RTF',
+    'File formats: MS Word, MS Excel, PDF, JPG, PNG, TXT, RTF, MP4, MP3',
     'File size per document: up to 20 megabytes (MB)',
+    'File size per multimedia file: up to 30 megabytes (MB)',
     'Files cannot be password protected',
   ],
   guideText:
-    'You cannot upload executable (.exe) or zip files because of virus risks. If you delete an uploaded file, you also delete the file description.',
+    'You can\'t upload executable (.exe), zip or other archive files due to virus risks.',
   titleForFile: 'Select documents to upload',
   uploadButton: 'Upload file',
   documentListLabel:
-    'List the description of each document uploaded on separate lines. For example, "1. Witness statement requested by the d2"',
+    'Please explain why you feel this document is relevant to your case.',
   delete: 'Delete',
   documentInLanguage: 'Document',
   fileuploaded: 'Files uploaded',
@@ -33,8 +38,7 @@ const englishContent = () => ({
   errors: {
     documentUpload: {
       noDocumentUploaded: 'Please choose a file.',
-      fileSize: `The file you uploaded is too large.
-              Maximum file size allowed is 20MB`,
+      fileSize: `File size exceeds the maximum permitted value. Please upload a file that is less than 20MB (documents) or less than 30MB (multimedia files)`,
       multimediaFileSize: 'The file you uploaded is too large. Maximum file size allowed is 500MB for multimedia files',
       fileFormat: `The file you uploaded is in the wrong format.
               Upload your file again in the correct format`,
@@ -54,29 +58,34 @@ const englishContent = () => ({
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 const welshContent = () => ({
-  serviceName: 'DSS Update Case',
-  title: 'Upload documents - welsh',
+  serviceName: 'Update an existing First-tier Tribunal case - welsh',
+  title: 'Add information to a case - welsh',
   paragraph1:
-    'You can upload additional documents, files or other evidence to support your case. You can also add information below that is relevant to the case. - welsh',
-  uploadedDocumentsSubTitle: 'Uploaded documents can include: - welsh',
+    'Please provide any additional relevant information that you would like to add to this case. Don\'t include any personal financial information, such as your banking details. - welsh',
+  additionalDocumentsHeading: 'Additional documents - welsh',
+  paragraph2: 'You may want to submit other documents, files, or evidence that provides the Tribunal with more information about your case. - welsh',
+  paragraph3: 'This will depend on the nature of your appeal, application or claim. Specific examples can be found in your completed tribunal form. - welsh',
+  uploadedDocumentsSubTitle: 'These may include: - welsh',
   uploadDocumentsBullets: [
     'copies of any relevant correspondence or documentation - welsh',
+    'audio or video recordings - welsh',
     'written statements - welsh',
     'related reports - welsh',
   ],
-  caseRelevancySubTitle: 'For each document uploaded you must state why it is relevant to the case. - welsh',
+  caseRelevancySubTitle: 'For each piece of additional information or evidence you are submitting, please state why it is relevant to your case. - welsh',
   fileUploadRequirementsLabel: 'File upload requirements - welsh',
   uploadHelpBullets: [
-    'File formats: MS Word, MS Excel, PDF, JPG, PNG, TXT, RTF - welsh',
+    'File formats: MS Word, MS Excel, PDF, JPG, PNG, TXT, RTF, MP4, MP3 - welsh',
     'File size per document: up to 20 megabytes (MB) - welsh',
+    'File size per multimedia file: up to 30 megabytes (MB) - welsh',
     'Files cannot be password protected - welsh',
   ],
   guideText:
-    'You cannot upload executable (.exe) or zip files because of virus risks. If you delete an uploaded file, you also delete the file description. - welsh',
+    'You can\'t upload executable (.exe), zip or other archive files due to virus risks. - welsh',
   titleForFile: 'Select documents to upload - welsh',
   uploadButton: 'Upload file - welsh',
   documentListLabel:
-    'List the description of each document uploaded on separate lines. For example, "1. Witness statement requested by the d2 - welsh"',
+    'Please explain why you feel this document is relevant to your case. - welsh"',
   delete: 'Delete - welsh',
   documentInLanguage: 'Document - welsh',
   fileuploaded: 'Files uploaded - welsh',
@@ -85,8 +94,7 @@ const welshContent = () => ({
   errors: {
     documentUpload: {
       noDocumentUploaded: 'Please choose a file. - welsh',
-      fileSize: `The file you uploaded is too large.
-              Maximum file size allowed is 20MB - welsh`,
+      fileSize: `File size exceeds the maximum permitted value. Please upload a file that is less than 20MB (documents) or less than 30MB (multimedia files) - welsh`,
       multimediaFileSize:
         'The file you uploaded is too large. Maximum file size allowed is 500MB for multimedia files - welsh',
       fileFormat: `The file you uploaded is in the wrong format.
