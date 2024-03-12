fb5ce3d665d7645d5849d13a1efa8ffe0a651d90
Tom Elliott <tomelliott@Toms-MacBook-Pro.local>
2024-03-12 18:32:06 +0000

    DTSSTCI-660: Add page view and routing for timed-out page

---
+++ src/main/steps/timed-out/get.test.ts
@@ -0,0 +1,20 @@
+import { mockRequest } from '../../../test/unit/utils/mockRequest';
+import { mockResponse } from '../../../test/unit/utils/mockResponse';
+
+import { TimedOutGetController } from './get';
+
+describe('TimedOutGetController', () => {
+  const controller = new TimedOutGetController();
+
+  test('Should destroy session and render timeout page', async () => {
+    const req = mockRequest();
+    const res = mockResponse();
+    await controller.get(req, res);
+
+    expect(req.session.destroy).toHaveBeenCalled();
+    expect(res.render).toHaveBeenCalledWith(
+      expect.stringContaining('/timed-out'),
+      expect.anything()
+    );
+  });
+});
---
+++ src/main/steps/timed-out/get.ts
@@ -0,0 +1,26 @@
+import autobind from 'autobind-decorator';
+import { Response } from 'express';
+
+import { AppRequest } from '../../app/controller/AppRequest';
+import { GetController } from '../../app/controller/GetController';
+
+import { generateContent } from './content';
+
+@autobind
+export class TimedOutGetController extends GetController {
+  constructor() {
+    super(__dirname + '/template', generateContent);
+  }
+
+  public async get(req: AppRequest, res: Response): Promise<void> {
+    res.locals['lang'] = req.session.lang;
+
+    req.session.destroy(err => {
+      if (err) {
+        throw err;
+      }
+
+      super.get(req, res);
+    });
+  }
+}
