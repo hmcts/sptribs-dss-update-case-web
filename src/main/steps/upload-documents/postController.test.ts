e38bd2b60e06a70d1507175639dd1565739500fa
gabriellemagruder <gabrielle.magruder@solirius.com>
2024-03-12 17:09:59 +0000

    Add missing unit tests for upload documents

--- src/main/steps/upload-documents/postController.test.ts
+++ src/main/steps/upload-documents/postController.test.ts
@@ -313,4 +313,46 @@ describe('Testing the post controller', () => {
     expect(req.session?.fileErrors[0].text).toEqual('You can only select up to 20 files at the same time');
     expect(req.session?.fileErrors[0].href).toEqual('#file-upload-1');
   });
+
+  test('Continuing without document upload or additional infomation', async () => {
+    const newRequest = req;
+    newRequest.body['documentDetail'] = '';
+    newRequest.session.caseDocuments = [];
+    newRequest.session['save'] = () => '';
+    newRequest.body.continue = true;
+    controller.post(newRequest, res);
+    expect(req.session?.fileErrors).toHaveLength(1);
+    expect(req.session?.fileErrors[0].text).toEqual('You cannot continue without providing additional information or a document');
+    expect(req.session?.fileErrors[0].href).toEqual('#file-upload-1');
+  });
+
+  test('Continuing with only additional infomation', async () => {
+    const newRequest = req;
+    newRequest.body['documentDetail'] = 'test';
+    newRequest.session.caseDocuments = [];
+    newRequest.session['save'] = () => '';
+    newRequest.body.continue = true;
+    controller.post(newRequest, res);
+    expect(req.session?.fileErrors).toHaveLength(0);
+  });
+
+  test('Continuing with only uploaded documents', async () => {
+    const newRequest = req;
+    newRequest.body['documentDetail'] = '';
+    newRequest.session.caseDocuments = ['doc1', 'doc2'];
+    newRequest.session['save'] = () => '';
+    newRequest.body.continue = true;
+    controller.post(newRequest, res);
+    expect(req.session?.fileErrors).toHaveLength(0);
+  });
+
+  test('Continuing with additional infomation and uploaded documents', async () => {
+    const newRequest = req;
+    newRequest.body['documentDetail'] = 'test';
+    newRequest.session.caseDocuments = ['doc1', 'doc2'];
+    newRequest.session['save'] = () => '';
+    newRequest.body.continue = true;
+    controller.post(newRequest, res);
+    expect(req.session?.fileErrors).toHaveLength(0);
+  });
 });
