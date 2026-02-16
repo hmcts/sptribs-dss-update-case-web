77b94b76a072ddef18df540b21f6d4beb5e505f7
P Revolta <phoebe.revolta@solirius.com>
2026-02-16 12:17:41 +0000

    DTSSTCI-1634: remove variations of x-frame-otpions and x-xss-protection headers

--- src/main/server.ts
+++ src/main/server.ts
@@ -60,7 +60,9 @@ app.use((req, res, next) => {
 });
 app.disable('x-powered-by');
 app.disable('X-Powered-By');
+app.disable('x-xss-protection');
 app.disable('X-Xss-Protection');
+app.disable('x-frame-options');
 app.disable('X-Frame-Options');
 app.use(cookies());
 
