d6460fce62dce5762d20e0c89456b99b875b2105
P Revolta <phoebe.revolta@solirius.com>
2026-02-16 16:49:55 +0000

    DTSSTCI-1634: remove x-frame-options and x-xss-protection via helmet

--- src/main/server.ts
+++ src/main/server.ts
@@ -60,10 +60,6 @@ app.use((req, res, next) => {
 });
 app.disable('x-powered-by');
 app.disable('X-Powered-By');
-app.disable('x-xss-protection');
-app.disable('X-Xss-Protection');
-app.disable('x-frame-options');
-app.disable('X-Frame-Options');
 app.use(cookies());
 
 const rateLimiterDisabled = process.env.RATE_LIMITER_DISABLED;
